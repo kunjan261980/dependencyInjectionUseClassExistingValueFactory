47c94572579bf8cb1610100dbd752ade99b8b875
kunjan261980 <kunjan261980@gmail.com>
2025-01-06 21:59:01 +0530

    useValue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,20 +2,21 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoggerService } from './logger.service';
 import { ExperimentalLoggerService } from './experimental-logger.service';
+import { LegacyLogger } from './legacy.logger';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  providers:[{provide: LoggerService , useExisting: ExperimentalLoggerService}]
+  providers:[{provide: LoggerService , useValue: LegacyLogger}]
 })
 export class AppComponent {
   title = 'angularTest';
   constructor(private logger: LoggerService,private explogger: ExperimentalLoggerService)
   {
-    this.logger.prefix = "app component"
+    //this.logger.prefix = "app component"
   this.logger.log("App Component Init")
-   console.log(this.logger === this.explogger)
+   //console.log(this.logger === this.explogger)
   }
 }
